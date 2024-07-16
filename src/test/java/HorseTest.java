cc4f15bf0898989c725fffa03d28f422b8aab250
Tamara Pavlova <tomik.9983@gmail.com>
2024-07-16 20:57:47 +0400

    add test Horse

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,11 +1,16 @@
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mockStatic;
+
 
 public class HorseTest {
 
@@ -46,4 +51,54 @@ public class HorseTest {
         });
         assertEquals("Distance cannot be negative.", exception.getMessage());
     }
+
+    @Test
+    void testGetName() {
+        String expectedName = "Test Name";
+        Horse testHorse = new Horse(expectedName, 1, 1);
+        assertEquals(expectedName, testHorse.getName());
+    }
+    @Test
+    void testGetSpeed() {
+        double expectedSpeed = 1.0;
+        Horse testHorse = new Horse("name", expectedSpeed, 1.0);
+        assertEquals(expectedSpeed, testHorse.getSpeed());
+    }
+    @Test
+    void testGetDistance() {
+        double expectedDistance = 1.0;
+        Horse testHorse = new Horse("name", 1.0, expectedDistance);
+        assertEquals(expectedDistance, testHorse.getDistance());
+    }
+    @Test
+    void testGetDistanceReturnNull() {
+        double expectedDistance = 0.0;
+        Horse testHorse = new Horse("name", 1.0);
+        assertEquals(expectedDistance, testHorse.getDistance());
+    }
+
+    @ExtendWith(MockitoExtension.class)
+    @Test
+    void testMoveCallsGetRandomDoubleWithParams() {
+        try (MockedStatic<Horse> mockedStatic = mockStatic(Horse.class)) {
+            double param1 = 0.2;
+            double param2 = 0.9;
+            Horse horseTest = new Horse("name", 1.0);
+            horseTest.move();
+            mockedStatic.verify(() -> Horse.getRandomDouble(param1, param2));
+        }
+    }
+    @Test
+    void testMoveAssignsCorrectDistanceValue() {
+        try (MockedStatic<Horse> horseMockedStatic = Mockito.mockStatic(Horse.class)) {
+            horseMockedStatic.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(0.5);
+            Horse horseTest = new Horse("name", 1.0, 1.0);
+            double distanceTest = horseTest.getDistance();
+            double expectedDistance =distanceTest+horseTest.getSpeed() * 0.5;
+            horseTest.move();
+            double actualDistance = distanceTest+horseTest.getSpeed()*0.5;
+            assertEquals(expectedDistance, actualDistance);
+        }
+    }
+
 }

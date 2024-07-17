d0ff818d9f8564708fd75b923a97b8eb033d6e89
Tamara Pavlova <tomik.9983@gmail.com>
2024-07-17 21:14:25 +0400

    add mainTest and refract HippodromeTest

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -12,34 +13,37 @@ public class HippodromeTest {
     private final String HORSES_CANNOT_BE_NULL = "Horses cannot be null.";
     private final String HORSES_CANNOT_BE_EMPTY = "Horses cannot be empty.";
 
-   @Test
+    @Test
     public void hippodromeConstructorTest() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, ()-> {
-           new Hippodrome(null);
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Hippodrome(null);
         });
         assertEquals(HORSES_CANNOT_BE_NULL, exception.getMessage());
     }
+
     @Test
     public void hippodromeConstructorListTest() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, ()-> {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
             new Hippodrome(new ArrayList<>());
         });
         assertEquals(HORSES_CANNOT_BE_EMPTY, exception.getMessage());
     }
+
     @Test
     public void getHorsesTest() {
         List<Horse> horsesListTest = new ArrayList<>();
-        for(int i=0; i<30; i++) {
+        for (int i = 0; i < 30; i++) {
             String name = "name" + i;
             horsesListTest.add(new Horse(name, i, i));
         }
         Hippodrome hippodromeTest = new Hippodrome(horsesListTest);
         assertIterableEquals(horsesListTest, hippodromeTest.getHorses());
     }
+
     @Test
     public void moveTest() throws Exception {
         List<Horse> mockHorses = new ArrayList<>();
-        for(int i=0; i<50; i++) {
+        for (int i = 0; i < 50; i++) {
             mockHorses.add(mock(Horse.class));
         }
         Hippodrome hippodromeTest = new Hippodrome(mockHorses);
@@ -51,15 +55,17 @@ public class HippodromeTest {
         }
 
     }
-
-    // Проверить, что метод вызывает метод move у всех лошадей.
-    // При создании объекта Hippodrome передай в конструктор список из 50 моков лошадей и воспользуйся методом verify
-    //   public void move() {
-    //       horses.forEach(Horse::move);
-    //   }
-//     <dependency>
-//            <groupId>net.bytebuddy</groupId>
-//            <artifactId>byte-buddy</artifactId>
-//            <version>1.14.17</version>
-//        </dependency>
+    @Test
+    public void getWinnerTest() {
+        List<Horse> horsesListTest = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            String name = "name" + i;
+            horsesListTest.add(new Horse(name, i, i));
+        }
+        Hippodrome hippodromeTest = new Hippodrome(horsesListTest);
+        assertEquals(horsesListTest.get(29), hippodromeTest.getWinner());
+    }
 }
+
+
+

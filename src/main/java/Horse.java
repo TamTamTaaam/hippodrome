e26f3e5587305f641e38fa62b8c83a9872bf4a25
Tamara Pavlova <tomik.9983@gmail.com>
2024-07-21 21:20:41 +0400

    add log

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,27 +1,35 @@
+import org.apache.logging.log4j.Logger;
+
 import static java.util.Objects.isNull;
 
 public class Horse {
 
+    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger("hippodrome");
     private final String name;
     private final double speed;
     private double distance;
 
     public Horse(String name, double speed, double distance) {
         if (isNull(name)) {
+            log.error("Horse: Name is null");
             throw new IllegalArgumentException("Name cannot be null.");
         } else if (name.isBlank()) {
+            log.error("Horse: Name is blank");
             throw new IllegalArgumentException("Name cannot be blank.");
         }
         if (speed < 0) {
+            log.error("Horse: Speed is negative");
             throw new IllegalArgumentException("Speed cannot be negative.");
         }
         if (distance < 0) {
+            log.error("Horse: Distance is negative");
             throw new IllegalArgumentException("Distance cannot be negative.");
         }
 
         this.name = name;
         this.speed = speed;
         this.distance = distance;
+        log.debug(String.format("Создание Horse, имя %s, скорость %.1f", name, speed));
     }
 
     public Horse(String name, double speed) {
@@ -41,7 +49,7 @@ public class Horse {
     }
 
     public void move() {
-        distance += speed * getRandomDouble(0.2, 0.9) ;
+        distance += speed * getRandomDouble(0.2, 0.9);
     }
 
     public static double getRandomDouble(double min, double max) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
-import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-@Log4j2
 public class Main {
+
+    private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
 
